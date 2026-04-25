07f3afefd1fc44223ce5f65afa9d9cd0c7e6cef9
404pranit <pranitkamb@gmail.com>
2026-04-25 05:59:18 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,21 +1,12 @@
-// DARK MODE
-const themeBtn = document.getElementById("themeBtn");
+const btn = document.getElementById("themeBtn");
 
 if(localStorage.getItem("theme")==="dark"){
   document.body.classList.add("dark");
 }
 
-themeBtn.onclick = ()=>{
+btn.onclick = ()=>{
   document.body.classList.toggle("dark");
   localStorage.setItem("theme",
     document.body.classList.contains("dark") ? "dark":"light"
   );
-};
-
-// MOBILE MENU
-const menuBtn = document.getElementById("menuBtn");
-const nav = document.getElementById("navLinks");
-
-menuBtn.onclick = ()=>{
-  nav.classList.toggle("active");
 };
