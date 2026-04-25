f01c60f153c89146dd6d5b9f63b4e2162ce1becb
404pranit <pranitkamb@gmail.com>
2026-04-25 05:43:01 +0530

    Refactor theme button and add mobile menu functionality

--- script.js
+++ script.js
@@ -1,12 +1,21 @@
-const btn = document.getElementById("themeBtn");
+// DARK MODE
+const themeBtn = document.getElementById("themeBtn");
 
 if(localStorage.getItem("theme")==="dark"){
   document.body.classList.add("dark");
 }
 
-btn.onclick = ()=>{
+themeBtn.onclick = ()=>{
   document.body.classList.toggle("dark");
   localStorage.setItem("theme",
     document.body.classList.contains("dark") ? "dark":"light"
   );
+};
+
+// MOBILE MENU
+const menuBtn = document.getElementById("menuBtn");
+const nav = document.getElementById("navLinks");
+
+menuBtn.onclick = ()=>{
+  nav.classList.toggle("active");
 };
